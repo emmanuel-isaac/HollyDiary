60e0a1b0e7379031c218eb862ced326bb1c44d7f
Isaac Ayodeji <deejecks@live.com>
2015-01-14 16:21:07 +0100

    changes made

--- bin/www.js
+++ bin/www.js
@@ -6,8 +6,4 @@ var app = require('./../app');
 //   console.log('Listening on port 3000');
 // });
 
-app.set('port', (process.env.PORT || 3000));
 
-app.listen(app.get('port', function () {
-  console.log('Listening on port 3000');
-}));
